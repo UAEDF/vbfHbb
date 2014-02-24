846e02a918dfbc5e48696b08acdd5330102e8470
salderwe <sara.alderweireldt@cern.ch>
2014-02-24 18:50:10 +0100

    Update.

--- master.sh
+++ master.sh
@@ -21,7 +21,7 @@ variables="-v mqq1,mbbReg1,dEtaqq1,mvaNOM,jetBtag00,jetBtag10,jetPt0,jetPt1,jetP
 outfile="-o trigger/rootfiles/trigger_Nmin1_NOM_bias.root"
 execute="--drawstack --closure --shade"
 
-if [ "$1" == "1" || "$1" == "0" ]; then
+if [ "$1" == "1" ] || [ "$1" == "0" ]; then
 	$prefix ./trigger/mkTurnonCurves.py $baseoptions $binstrigger $trigger $selection $weight $samples $variables $outfile $execute
 fi
 
@@ -32,13 +32,13 @@ $prefix
 #********************
 outfile="-o trigger/rootfiles/trigger_2DMaps_NOM_jetBtag00-jetBtag10.root"
 mapstring="-m jetBtag00;0.0#0.244#0.679#0.898#1.001,jetBtag10;0.0#0.244#0.679#0.898#1.001"
-if [ "$1" == "2" || "$1" == "0" ]; then
+if [ "$1" == "2" ] || [ "$1" == "0" ]; then
 	$prefix ./common/main.py $baseoptions $trigger $selection $weight $samples $outfile $mapstring
 fi
 
 outfile="-o trigger/rootfiles/trigger_2DMaps_NOM_jetBtag00-mqq1.root"
 mapstring="-m jetBtag00;0.0#0.244#0.679#0.898#1.001,mqq1;0#250#450#700#1200#2000"
-if [ "$1" == "2" || "$1" == "0" ]; then
+if [ "$1" == "2" ] || [ "$1" == "0" ]; then
 	$prefix ./common/main.py $baseoptions $trigger $selection $weight $samples $outfile $mapstring
 fi
 
@@ -51,13 +51,13 @@ execute="--drawstack --overlay --shade"
 
 outfile="-o trigger/rootfiles/trigger_Nmin1_NOM_closure.root"
 weight="-w 19800.,XSEC;LUMI;PU#3;MAP#jetBtag[b1[0]]#jetBtag[b2[0]],,trigger/rootfiles/trigger_2DMaps_NOM_jetBtag00-jetBtag10.root;2DMaps/JetMon-QCD/2DMap_JetMon-QCD-Rat_sBtag0_LL-dEtaqq0_gt2p5-jetPt0_gt80-jetPt1_gt70-jetPt2_gt50-jetPt3_gt40-mqq1_gt250-tNOMMC-rAV80-dNOMMC_jetBtag00-jetBtag10"
-if [ "$1" == "3" || "$1" == "0" ]; then
+if [ "$1" == "3" ] || [ "$1" == "0" ]; then
 	$prefix ./trigger/mkTurnonCurves.py $baseoptions $binstrigger $trigger $selection $weight $samples $variables $outfile $execute
 fi
 
 outfile="-o trigger/rootfiles/trigger_Nmin1_NOM_closure.root"
 weight="-w 19800.,XSEC;LUMI;PU#3;MAP#jetBtag[b1[0]]#mqq[1],,trigger/rootfiles/trigger_2DMaps_NOM_jetBtag00-mqq1.root;2DMaps/JetMon-QCD/2DMap_JetMon-QCD-Rat_sBtag0_LL-dEtaqq0_gt2p5-jetPt0_gt80-jetPt1_gt70-jetPt2_gt50-jetPt3_gt40-mqq1_gt250-tNOMMC-rAV80-dNOMMC_jetBtag00-mqq1"
-if [ "$1" == "4" || "$1" == "0" ]; then
+if [ "$1" == "4" ] || [ "$1" == "0" ]; then
 	$prefix ./trigger/mkTurnonCurves.py $baseoptions $binstrigger $trigger $selection $weight $samples $variables $outfile $execute
 fi
 
@@ -73,17 +73,17 @@ outfile="-o plots/rootfiles/control_NOM.root"
 execute="--drawstack"
 
 weight="-w 19800.,XSEC;LUMI;PU#0;KFAC"
-if [ "$1" == "5" || "$1" == "0" ]; then
+if [ "$1" == "5" ] || [ "$1" == "0" ]; then
 	$prefix ./plots/mkPlots.py $baseoptions $binscomparison $trigger $trigger $selection $weight $samples $variables $outfile $execute 
 fi
 
 weight="-w 19800.,XSEC;LUMI;PU#0;KFAC;MAP#jetBtag[b1[0]]#jetBtag[b2[0]],,trigger/rootfiles/trigger_2DMaps_NOM_jetBtag00-jetBtag10.root;2DMaps/JetMon-QCD/2DMap_JetMon-QCD-Rat_sBtag0_LL-dEtaqq0_gt2p5-jetPt0_gt80-jetPt1_gt70-jetPt2_gt50-jetPt3_gt40-mqq1_gt250-tNOMMC-rAV80-dNOMMC_jetBtag00-jetBtag10"
-if [ "$1" == "6" || "$1" == "0" ]; then
+if [ "$1" == "6" ] || [ "$1" == "0" ]; then
 	$prefix ./plots/mkPlots.py $baseoptions $binscomparison $trigger $trigger $selection $weight $samples $variables $outfile $execute 
 fi
 
 weight="-w 19800.,XSEC;LUMI;PU#0;KFAC;MAP#jetBtag[b1[0]]#mqq[1],,trigger/rootfiles/trigger_2DMaps_NOM_jetBtag00-mqq1.root;2DMaps/JetMon-QCD/2DMap_JetMon-QCD-Rat_sBtag0_LL-dEtaqq0_gt2p5-jetPt0_gt80-jetPt1_gt70-jetPt2_gt50-jetPt3_gt40-mqq1_gt250-tNOMMC-rAV80-dNOMMC_jetBtag00-mqq1"
-if [ "$1" == "7" || "$1" == "0" ]; then
+if [ "$1" == "7" ] || [ "$1" == "0" ]; then
 	$prefix ./plots/mkPlots.py $baseoptions $binscomparison $trigger $trigger $selection $weight $samples $variables $outfile $execute 
 fi
 
