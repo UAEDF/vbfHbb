16726a2d8a46ae243aeb16d9272160d3dc833aa5
salderwe <sara.alderweireldt@cern.ch>
2014-05-22 13:55:53 +0200

    Local update.

---
+++ uncertainties/master_JEx.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+#./mkUncJEx.py -D ../common/vbfHbb_defaultOpts_2013.json -G /data/UAData/JEx -o rootfiles/vbfHbb_uncertainties_JEx.root -s "VBF125" -p "Btag0_LL;dEtaqq1_gt2p5;dPhibb1_lt2;jetPt0_gt80;jetPt1_gt70;jetPt2_gt50;jetPt3_gt40;mqq1_gt250;nLeptons" -t "NOMMC" --tag "NOM" --weight "19800.,XSEC;LUMI;PU#0" -v "mbbReg1" --binning "mbbReg1;300;50;200"
